cb48a12fddf882b472169f94055a69fe904373f4
Jason <jaysnmury@gmail.com>
2024-03-26 23:46:12 +0300

    support for automated data download

--- cgan_ui/show_forecasts.py
+++ cgan_ui/show_forecasts.py
@@ -35,14 +35,14 @@ var_info = {
         "offset": 0,
         "accumulated": False,
     },
-    "t2m": {
+    "2t": {
         "name": "Two metre temperature",
         "units": "deg. C",
         "normalisation": 1,
         "offset": -273.15,  # Convert from Kelvin to deg. C
         "accumulated": False,
     },
-    "wind": {
+    "ws": {
         "name": "Wind speed",
         "units": "m/s",
         "normalisation": 1,
