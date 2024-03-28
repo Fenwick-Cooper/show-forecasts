744f346bd588541a8b1797a3f200cf2afe918746
Jason <jaysnmury@gmail.com>
2024-03-28 05:18:50 +0300

    running data download jobs from 8am Nairobi time

--- cgan_ui/jobs.py
+++ cgan_ui/jobs.py
@@ -3,11 +3,12 @@ import time
 from datetime import datetime
 from cgan_ui.download import download_ifs_forecast_data
 
-for hour in range(3, 23, 1):
-    schedule.every().day.at(f"{str(hour).rjust(2, '0')}:00", "Europe/London").do(
+for hour in range(8, 23, 1):
+    schedule.every().day.at(f"{str(hour).rjust(2, '0')}:00", "Africa/Nairobi").do(
         download_ifs_forecast_data, date_str=datetime.now().strftime("%Y-%m-%d")
     )
 
+print(schedule.get_jobs())
 while True:
     all_jobs = schedule.get_jobs()
     schedule.run_pending()
