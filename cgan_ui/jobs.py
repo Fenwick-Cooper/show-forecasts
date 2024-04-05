6be66367be8e3cbcfccdd26f51036877f0e262ac
Jason <jaysnmury@gmail.com>
2024-04-05 05:34:01 +0300

    added a one-off data downloads execution step on jobs initialization

--- cgan_ui/jobs.py
+++ cgan_ui/jobs.py
@@ -4,6 +4,9 @@ from datetime import datetime
 from cgan_ui.download import download_ifs_forecast_data
 from loguru import logger
 
+logger.info("one-time-off execution of function download_ifs_forecast_data")
+download_ifs_forecast_data()
+
 for hour in range(8, 23, 1):
     schedule.every().day.at(f"{str(hour).rjust(2, '0')}:00", "Africa/Nairobi").do(
         download_ifs_forecast_data, date_str=datetime.now().strftime("%Y-%m-%d")
