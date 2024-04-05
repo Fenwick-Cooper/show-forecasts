cac3908c3967b871056de976e03cb8dd891fdce5
Jason <jaysnmury@gmail.com>
2024-04-05 05:22:05 +0300

    modified jobs queue strategy

--- cgan_ui/jobs.py
+++ cgan_ui/jobs.py
@@ -2,13 +2,16 @@ import schedule
 import time
 from datetime import datetime
 from cgan_ui.download import download_ifs_forecast_data
+from loguru import logger
 
 for hour in range(8, 23, 1):
     schedule.every().day.at(f"{str(hour).rjust(2, '0')}:00", "Africa/Nairobi").do(
         download_ifs_forecast_data, date_str=datetime.now().strftime("%Y-%m-%d")
     )
 
-print(schedule.get_jobs())
+for job in schedule.get_jobs():
+    logger.info(f"scheduled ecmwf data download task {job}")
+
 while True:
     all_jobs = schedule.get_jobs()
     schedule.run_pending()
