52ad2fc3b5edd880060baa17566aae4b55adca2b
Jason <jaysnmury@gmail.com>
2024-04-08 22:13:33 +0300

    service code cleanup

--- cgan_ui/download.py
+++ cgan_ui/download.py
@@ -145,7 +145,7 @@ def post_process_downloaded_ecmwf_forecasts(
     source: str | None = "ecmwf", stream: str | None = "enfo"
 ) -> None:
     grib2_files = sorted(get_forecast_data_files(source=source, stream=stream))
-    logger.info(f"starting batch post-processing tasks for {', '.join(grib2_files)}")
+    logger.info(f"starting batch post-processing tasks for {'\n'.join(grib2_files)}")
     for grib2_file in grib2_files:
         post_process_ecmwf_grib2_dataset(
             source=source, stream=stream, grib2_file_name=grib2_file
