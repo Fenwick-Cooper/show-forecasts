cb60ebbdd914c9f2f0d6b7a19e608eb888c3400a
Fenwick Cooper <Fenwick.Cooper@physics.ox.ac.uk>
2024-04-26 20:37:07 +0100

    Added plot of 24h rainfall to GAN forecasts

--- show_forecasts/show_cGAN.py
+++ show_forecasts/show_cGAN.py
@@ -3,7 +3,8 @@
 # To do:
 #   Change "mm h**-1" to "mm/h" in the data.
 #   Store the model name in the data.
-#   Combine with the show_forecasts.py script used for plotting IFS open data.
+#   Add 24h plot or select time index.
+#   Check time zone and make label.
 
 import numpy as np
 import cartopy.feature as cfeature
@@ -16,7 +17,6 @@ from matplotlib.colors import ListedColormap
 from datetime import datetime, timedelta
 import cfgrib
 import xarray as xr
-#from .data_utils import datetime64_to_datetime, get_contour_levels, get_region_extent, get_plot_normalisation
 from .data_utils import *
 
 
@@ -119,10 +119,92 @@ def plot_GAN_forecast(data, style=None, plot_units='mm/h', region='ICPAC'):
         plt.show()  # Finally draw the plot
 
 
+# Plot the ensemble mean and ensemble standard deviation of the cGAN forecast data
+# at each valid time.
+# Arguments
+#   data              - An xarray DataSet containing the cGAN rainfall forecasts.
+#   style=None        - Options: 'ICPAC', 'ICPAC_heavy', 'KMD', 'EMI', 'EMI_heavy'
+#   plot_units='mm/h' - Can be 'mm/h' (default), 'mm/6h', 'mm/day' or 'mm/week'
+#   region='ICPAC'    - can be 'ICPAC', 'Kenya', 'South Sudan', 'Rwanda', 'Burundi', 'Djibouti',
+#                       'Eritrea', 'Ethiopia', 'Sudan', 'Somalia', 'Tanzania', 'Uganda'
+def plot_GAN_forecast_24h(data, style=None, plot_units='mm/h', region='ICPAC'):
+    
+    # Get the units to use for plotting
+    plot_norm = get_plot_normalisation(plot_units)
+    
+    # To be consistent with the Harris et. al paper.
+    value_range_precip = (0.1, 15 * plot_norm)
+    
+    # Use a style other than the default
+    if (style is not None):
+        plot_levels, plot_colours = get_contour_levels(style)
+    
+    # Load the border shapefile
+    reader = shpreader.Reader("show_forecasts/GHA_shapes/gha.shp")
+    shape_feature = ShapelyFeature(reader.geometries(), ccrs.PlateCarree(), facecolor='none')
+    
+    # Get the extent of the region that we are looking at
+    if (region != 'ICPAC'):
+        region_extent = get_region_extent(region, border_size=0.5)
+
+    # Convert the forecast valid time to a datetime.datetime format
+    valid_time = datetime64_to_datetime(data['fcst_valid_time'][0,0].values)
+
+    # Define the figure and each axis for the rows and columns
+    fig, axs = plt.subplots(nrows=1, ncols=2, subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(10,5))
+
+    # axs is a 2 dimensional array of `GeoAxes`. Flatten it into a 1-D array
+    axs=axs.flatten()
+
+    ax=axs[0]  # First plot (left)
+    ax.add_feature(cfeature.COASTLINE, linewidth=1)  # Draw some features to see where we are
+    ax.add_feature(shape_feature)  # The borders
+    ax.add_feature(cfeature.LAKES, linewidth=1,linestyle='-',edgecolor='dimgrey',facecolor='none')
+    # Actually make the plot
+    if (style == None):
+        c = ax.pcolormesh(data['longitude'], data['latitude'],
+                          np.mean(data['precipitation'][0,:,:,:,:], axis=(0,1)) * plot_norm,
+                          norm=colors.LogNorm(*value_range_precip), cmap='YlGnBu', transform=ccrs.PlateCarree())
+        cb = plt.colorbar(c, fraction=0.04)  # Add a colorbar with a nice size
+    else:
+        c = ax.contourf(data['longitude'], data['latitude'],
+                        np.mean(data['precipitation'][0,:,:,:,:], axis=(0,1)) * plot_norm,
+                        colors=plot_colours, levels=plot_levels*plot_norm, transform=ccrs.PlateCarree())
+        cb = plt.colorbar(c, fraction=0.04, ticks=plot_levels*plot_norm)  # Add a colorbar with a nice size
+    if (region != 'ICPAC'):
+        ax.set_extent(region_extent, crs=ccrs.PlateCarree())
+    cb.set_label(f'Rainfall ({plot_units})')  # Label the colorbar
+    ax.set_title(f"Ensemble mean",size=14)  # This plot's title
+
+    ax=axs[1]  # Second plot (right)
+    ax.add_feature(cfeature.COASTLINE, linewidth=1)  # Draw some features to see where we are
+    ax.add_feature(shape_feature)  # The borders
+    ax.add_feature(cfeature.LAKES, linewidth=1,linestyle='-',edgecolor='dimgrey',facecolor='none')
+    # Actually make the plot
+    if (style == None):
+        c = ax.pcolormesh(data['longitude'], data['latitude'],
+                          np.sqrt(np.mean(np.var(data['precipitation'][0,:,:,:,:], axis=0, ddof=1), axis=0)) * plot_norm,
+                          norm=colors.LogNorm(*value_range_precip), cmap='YlGnBu', transform=ccrs.PlateCarree())
+        cb = plt.colorbar(c, fraction=0.04)  # Add a colorbar with a nice size
+    else:
+        c = ax.contourf(data['longitude'], data['latitude'],
+                        np.sqrt(np.mean(np.var(data['precipitation'][0,:,:,:,:], axis=0, ddof=1), axis=0)) * plot_norm,
+                        colors=plot_colours, levels=plot_levels*plot_norm, transform=ccrs.PlateCarree())
+        cb = plt.colorbar(c, fraction=0.04, ticks=plot_levels*plot_norm)  # Add a colorbar with a nice size
+    if (region != 'ICPAC'):
+        ax.set_extent(region_extent, crs=ccrs.PlateCarree())
+    cb.set_label(f'Rainfall ({plot_units})')  # Label the colorbar
+    ax.set_title(f"Ensemble standard deviation",size=14)  # This plot's title
+
+    fig.suptitle(f"Jurre Brishti cGAN forecast: Valid {valid_time} - {valid_time + timedelta(days=1)}")  # Overall title
+    plt.tight_layout()  # Looks nicer
+    plt.show()  # Finally draw the plot
+        
+        
 # Plot all ensemble members in the cGAN forecast data at a specified valid time.
 # Arguments
 #   data                  - An xarray DataSet containing the cGAN rainfall forecasts.
-#   valid_time_start_hour - The hour the valid time starts at. Can either be 6, 12, 18 or 0.
+#   valid_time_start_hour - The hour the valid time starts at. Can either be 6, 12, 18 or 0 UTC.
 #   style=None            - Options: 'ICPAC', 'ICPAC_heavy', 'KMD', 'EMI', 'EMI_heavy'
 #   plot_units='mm/h'     - Can be 'mm/h' (default), 'mm/6h', 'mm/day' or 'mm/week'
 #   region='ICPAC'        - can be 'ICPAC', 'Kenya', 'South Sudan', 'Rwanda', 'Burundi', 'Djibouti',
